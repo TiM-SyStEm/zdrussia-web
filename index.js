437f54722c0bd49ef9067e2a9573177ddac65fe2
Timofei Gorlov <72683799+TiM-SyStEm@users.noreply.github.com>
2023-10-08 23:07:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+const li0 = document.getElementById("li1");
+const li2 = document.getElementById("li2");
+const li3 = document.getElementById("li3");
+const li4 = document.getElementById("li4");
+const li5 = document.getElementById("li5");
+const li6 = document.getElementById("li6");
+const li7 = document.getElementById("li7");
+li0.addEventListener(
+  "mouseover",
+  (event) => {
+    document.getElementById("snipet").style.display = "inline";
+    document.getElementById("snipet").innerHTML = "После покупки игры вы получаете возможность играть, как на Android-устройствах, так и на Windows и Linux. Ваш прогресс сохраняется в облаке.";
+  },
+  false,
+);
+li0.addEventListener(
+  "mouseout",
+  (event) => {
+    document.getElementById("snipet").style.display = "none";
+  },
+  false,
+);
+li2.addEventListener(
+  "mouseover",
+  (event) => {
+    document.getElementById("snipet").style.display = "inline";
+    document.getElementById("snipet").innerHTML = "Давай дальше";
+  },
+  false,
+);
+li2.addEventListener(
+  "mouseout",
+  (event) => {
+    document.getElementById("snipet").style.display = "none";
+  },
+  false,
+);
+li3.addEventListener(
+  "mouseover",
+  (event) => {
+    document.getElementById("snipet").style.display = "inline";
+    document.getElementById("snipet").innerHTML = "Еще чуть чуть";
+  },
+  false,
+);
+li3.addEventListener(
+  "mouseout",
+  (event) => {
+    document.getElementById("snipet").style.display = "none";
+  },
+  false,
+);
+li4.addEventListener(
+  "mouseover",
+  (event) => {
+    document.getElementById("snipet").style.display = "inline";
+    document.getElementById("snipet").innerHTML = "Еще немножка";
+  },
+  false,
+);
+li4.addEventListener(
+  "mouseout",
+  (event) => {
+    document.getElementById("snipet").style.display = "none";
+  },
+  false,
+);
+li5.addEventListener(
+  "mouseover",
+  (event) => {
+    document.getElementById("snipet").style.display = "inline";
+    document.getElementById("snipet").innerHTML = "Просто потерпи";
+  },
+  false,
+);
+li5.addEventListener(
+  "mouseout",
+  (event) => {
+    document.getElementById("snipet").style.display = "none";
+  },
+  false,
+);
+li6.addEventListener(
+  "mouseover",
+  (event) => {
+    document.getElementById("snipet").style.display = "inline";
+    document.getElementById("snipet").innerHTML = "СЛЕДУЮЩЕЕЕ!!!";
+  },
+  false,
+);
+li6.addEventListener(
+  "mouseout",
+  (event) => {
+    document.getElementById("snipet").style.display = "none";
+  },
+  false,
+);
+li7.addEventListener(
+  "mouseover",
+  (event) => {
+    document.getElementById("snipet").style.display = "inline";
+    document.getElementById("snipet").innerHTML = "Нет, не сюжет, реальная жизнь! Устал подчиняться начальству на работе? Подчиняйся начальству в игре. Вы начинаете игру молодым помощником машиниста в одном Российском депо, заканчиваете игру, а в прочем не важно ...";
+  },
+  false,
+);
+li7.addEventListener(
+  "mouseout",
+  (event) => {
+    document.getElementById("snipet").style.display = "none";
+  },
+  false,
+);
